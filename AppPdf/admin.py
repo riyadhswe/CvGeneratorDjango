268bffaba8210e73424c1382f60b98b31d2d5a11
riyadhswe <riyadhswe@gmail.com>
2022-04-09 20:31:35 +0600

    Admin Pannel Custom

--- AppPdf/admin.py
+++ AppPdf/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.site_header = "Pdf Genarator"
+admin.site.site_title = "Pdf Genarator"
+admin.site.index_title = "Pdf Genarator"
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name',)
+    fields = ('name','email')
+    list_editable = ('email',)
+
+
+admin.site.register(Profile,ProfileAdmin)
