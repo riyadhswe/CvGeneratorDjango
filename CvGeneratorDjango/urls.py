c3dd47d63b739367ae814b356f6101a4b9c7b94f
riyadhswe <riyadhswe@gmail.com>
2022-04-09 16:25:10 +0600

    End The Project

--- CvGeneratorDjango/urls.py
+++ CvGeneratorDjango/urls.py
@@ -5,4 +5,6 @@ from AppPdf import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.accept, name="accept"),
+    path('<int:id>', views.resume, name="resume"),
+    path('list/', views.list, name="list"),
 ]
